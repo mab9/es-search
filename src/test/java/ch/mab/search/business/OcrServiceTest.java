0733b8574ca719c946900c1320c3191ea57f0398
marc-antoine <marcantoine.bruelhart@gmail.com>
2019-04-09 14:33:17 +0200

    fix failing tests - adjust resources path and springboot junit 5 test setup

--- src/test/java/ch/mab/search/business/OcrServiceTest.java
+++ src/test/java/ch/mab/search/business/OcrServiceTest.java
@@ -1,16 +1,15 @@
 package ch.mab.search.business;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
 import java.io.IOException;
 
 import static junit.framework.TestCase.assertTrue;
 
-@RunWith(SpringRunner.class)
+@SpringBootTest
 public class OcrServiceTest {
 
 
@@ -19,21 +18,21 @@ public class OcrServiceTest {
 
     @Test
     public void extractTextFromFile_pdfWithoutImage_fullText() throws IOException {
-        File file = new File("src/test/java/futurama.pdf");
+        File file = new File("src/test/resources/futurama.pdf");
         String txt = service.extractTextFromFile(file);
         assertTrue(!txt.isEmpty());
     }
 
     @Test
     public void extractTextFromFile_pdfWithImageComputerLetters_fullText() throws IOException {
-        File file = new File("src/test/java/LE_Tasks.pdf");
+        File file = new File("src/test/resources/LE_Tasks.pdf");
         String txt = service.extractTextFromFile(file);
         assertTrue(!txt.isEmpty());
     }
 
     @Test
     public void extractTextFromFile_pdfWithImageHandWrittenLetters_fullText() throws IOException {
-        File file = new File("src/test/java/04 SOLID Prinzipien 19F 4Ia.pdf");
+        File file = new File("src/test/resources/04 SOLID Prinzipien 19F 4Ia.pdf");
         String txt = service.extractTextFromFile(file);
         assertTrue(!txt.isEmpty());
     }
