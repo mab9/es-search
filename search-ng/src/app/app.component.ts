89d783a8b1ebf2a7699eeea86c83267295629efe
marc-antoine <marcantoine.bruelhart@gmail.com>
2019-04-27 15:24:21 +0200

    add simple contact bar and contact list

--- search-ng/src/app/app.component.ts
+++ search-ng/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Contact} from "./models/contact";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'search-ng';
+  contact: Contact = {
+    id: 6,
+    name: 'Diana Ellis',
+    email: '',
+    phone: '',
+    birthday: '',
+    website: '',
+    image: '/assets/images/6.jpg',
+    address: {
+      street: '6554 park lane',
+      zip: '43378',
+      city: 'Rush',
+      country: 'United States'
+    }
+  }
 }
--- search-ng/src/app/app.module.ts
+++ search-ng/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {DocumentsMaterialModule} from "./documents.-material.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    DocumentsMaterialModule,
     AppRoutingModule
   ],
   providers: [],
