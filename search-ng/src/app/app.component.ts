7ee759453749ba8525d8074b77f88d6ccb4d58fb
marc-antoine <marcantoine.bruelhart@gmail.com>
2019-04-27 15:29:55 +0200

    make use of template and contact list

--- search-ng/src/app/app.component.ts
+++ search-ng/src/app/app.component.ts
@@ -1,25 +1,21 @@
 import { Component } from '@angular/core';
 import {Contact} from "./models/contact";
+import {CONTACT_DATA} from "./data/contact-data";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div style="text-align:center">
+      <mat-toolbar color="primary">Contacts</mat-toolbar>
+      <mat-list>
+        <mat-list-item *ngFor="let item of contacts ; trackBy: trackByContacts">
+          <img mat-list-avatar [src]="item.image" alt="{{item.name}}">
+          <h3 mat-line>{{ item.name }}</h3></mat-list-item>
+      </mat-list>
+    </div>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  contact: Contact = {
-    id: 6,
-    name: 'Diana Ellis',
-    email: '',
-    phone: '',
-    birthday: '',
-    website: '',
-    image: '/assets/images/6.jpg',
-    address: {
-      street: '6554 park lane',
-      zip: '43378',
-      city: 'Rush',
-      country: 'United States'
-    }
-  }
+  contacts: Contact[] = CONTACT_DATA;
+  trackByContacts(index: number, contact: Contact): number | string { return contact.id; }
 }
