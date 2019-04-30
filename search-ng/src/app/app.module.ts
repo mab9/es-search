67c6d489be2bdaca489aeea1314f21deaf4cf5ae
marc-antoine <marcantoine.bruelhart@gmail.com>
2019-04-30 22:01:11 +0200

    Remove unused imports

--- search-ng/src/app/app.module.ts
+++ search-ng/src/app/app.module.ts
@@ -1,26 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {AppMaterialModule} from "./app-material.module";
 import {ContactsService} from "./contacts/contacts.service";
-import { ContactsListComponent } from './contacts/contacts-list/contacts-list.component';
+import {ContactsListComponent} from './contacts/contacts-list/contacts-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import {RouterModule} from "@angular/router";
 import {APP_ROUTES} from "./app.routes";
-import { ContactsDetailComponent } from './contacts/contacts-detail/contacts-detail.component';
-import { ContactsDetailViewComponent } from './contacts/contacts-detail-view/contacts-detail-view.component';
-import { ContactsEditorComponent } from './contacts/contacts-editor/contacts-editor.component';
+import {ContactsDetailComponent} from './contacts/contacts-detail/contacts-detail.component';
+import {ContactsDetailViewComponent} from './contacts/contacts-detail-view/contacts-detail-view.component';
+import {ContactsEditorComponent} from './contacts/contacts-editor/contacts-editor.component';
 import {FormsModule} from "@angular/forms";
-import { ContactsDashboardComponent } from './contacts/contacts-dashboard/contacts-dashboard.component';
-import { AboutComponent } from './about/about.component';
-import { TabComponent } from './shared/tab/tab.component';
-import { TabsComponent } from './shared/tabs/tabs.component';
-import { DocumentSearchComponent } from './documents/document-search/document-search.component';
+import {ContactsDashboardComponent} from './contacts/contacts-dashboard/contacts-dashboard.component';
+import {AboutComponent} from './about/about.component';
+import {TabComponent} from './shared/tab/tab.component';
+import {TabsComponent} from './shared/tabs/tabs.component';
+import {DocumentSearchComponent} from './documents/document-search/document-search.component';
 import {DocumentService} from "./documents/document.service";
-import {MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -51,4 +50,5 @@ import {MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateMo
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
